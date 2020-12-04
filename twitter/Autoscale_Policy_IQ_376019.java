821e74331f100983fb7be0f5e9d6c5cb14f966d1
Bhavesh Anand <bhaveshanand7@gmail.com>
2020-12-04 17:35:56 +0530

    Create Autoscale_Policy_IQ_376019.java

---
+++ twitter/Autoscale_Policy_IQ_376019.java
@@ -0,0 +1,26 @@
+public class Autoscale_Policy_IQ_376019 {
+    
+    private static int getInstances(int instances, int[] utilization){
+        
+        int i = 0, n = utilization.length;
+        while (i < n){
+            if (utilization[i] < 25 && instances > 1){
+                instances = (int) Math.ceil(instances / 2);
+                i += 10;
+                continue;
+            } else if (utilization[i] > 60 && (2 * instances < 2 * Math.pow(10, 8))){
+                instances *= 2;
+                i += 10;
+                continue;
+            }
+            i++;
+        }
+        return instances;
+    }
+    
+    public static void main(String[] args) {
+        
+        int instances = 1;
+        System.out.println(getInstances(instances, new int[]{5, 10, 80}));
+    }
+}
